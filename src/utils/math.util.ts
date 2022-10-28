55118eeafb43d34038c9a7c5d94dadb57d7a69bc
Kamil Janiszewski <kamil.janiszewski@softiq.pl>
2022-10-28 02:40:19 +0200

    Fix the error with sometimes returning out-of-range values.

--- src/utils/math.util.ts
+++ src/utils/math.util.ts
@@ -1,5 +1,7 @@
 export function getRandomIntNumber(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min) * min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function isNumberInRange(number: number, min: number, max: number): boolean {
